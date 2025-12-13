d834f696b413be0e6e6d2cade78eaaec42d0c7ba
Ronak Gondaliya <gondaliyaronak78@gmail.com>
2025-12-14 05:12:43 +0530

    Initializing

---
+++ ShopHub/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ShopHubAuthPage from './component/ShopHubAuthPage'
+
+function App() {
+
+  return (
+    <>
+    <ShopHubAuthPage/>
+    </>
+  )
+}
+
+export default App
---
+++ ShopHubBackend/src/main/java/com/ronak/shophubbackend/ShopHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ronak.shophubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopHubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopHubBackendApplication.class, args);
+    }
+
+}
