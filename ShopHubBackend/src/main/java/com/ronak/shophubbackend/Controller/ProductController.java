a7f6607a884b223b1c2e76492731db12a1f9d4c8
Ronak Gondaliya <gondaliyaronak78@gmail.com>
2025-12-15 10:14:39 +0530

    Add getProduct

--- ShopHubBackend/src/main/java/com/ronak/shophubbackend/Controller/ProductController.java
+++ ShopHubBackend/src/main/java/com/ronak/shophubbackend/Controller/ProductController.java
@@ -1,12 +1,23 @@
 package com.ronak.shophubbackend.Controller;
 
+import com.ronak.shophubbackend.Model.Product;
 import com.ronak.shophubbackend.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class ProductController {
 
     @Autowired
     ProductService productService;
+
+    @GetMapping("/getproducts")
+    public List<Product> getList(){
+        return productService.getList();
+    }
+    
 }
--- ShopHubBackend/src/main/java/com/ronak/shophubbackend/Service/ProductService.java
+++ ShopHubBackend/src/main/java/com/ronak/shophubbackend/Service/ProductService.java
@@ -1,12 +1,19 @@
 package com.ronak.shophubbackend.Service;
 
+import com.ronak.shophubbackend.Model.Product;
 import com.ronak.shophubbackend.Repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
 
     @Autowired
     ProductRepo productRepo;
+
+    public List<Product> getList(){
+        return productRepo.findAll();
+    }
 }
